0baea5d43a8d6d0121135b5d0059ee6b040c09bd
Frédéric Fer <f.fer@institut-myologie.org>
2026-02-15 18:04:41 +0100

    improve(matlab-list): wrap Python bridge errors with clear setup guidance

--- matlab/+gaitkit/listMethods.m
+++ matlab/+gaitkit/listMethods.m
@@ -1,8 +1,19 @@
 function methods = listMethods()
 %LISTMETHODS Return available detector methods.
 
-jsonMod = py.importlib.import_module('json');
-pyMethods = py.gaitkit.list_methods();
-methodsJson = char(jsonMod.dumps(pyMethods));
-methods = string(jsondecode(methodsJson));
+try
+    jsonMod = py.importlib.import_module('json');
+    gaitkitMod = py.importlib.import_module('gaitkit');
+catch ME
+    error(['Could not import Python gaitkit. Configure pyenv and install gaitkit ', ...
+           'in that interpreter. Original error: %s'], ME.message);
+end
+
+try
+    pyMethods = gaitkitMod.list_methods();
+    methodsJson = char(jsonMod.dumps(pyMethods));
+    methods = string(jsondecode(methodsJson));
+catch ME
+    error('Failed to retrieve methods from Python gaitkit: %s', ME.message);
+end
 end
